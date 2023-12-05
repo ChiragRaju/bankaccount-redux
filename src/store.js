8589bf4cb8632fe42c65254e4e5d619099c156c0
Chirag Raju <chiragrajus2002@gmail.com>
2023-12-06 00:47:39 +0530

    till action creaters

--- src/store.js
+++ src/store.js
@@ -38,4 +38,6 @@ const requestLoan=(amount,purpose)=>{
 return {type:"account/requestLoan",payload:{amount,purpose}}
 
 }
-const payLoan=()=>{}
+const payLoan=()=>{
+  return {type:"account/payLoan"}
+}
